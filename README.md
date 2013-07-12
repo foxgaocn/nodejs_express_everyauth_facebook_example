872ba65671f110e2ba326b9f4ff7c6ac33fcb746
Haiyang (harryg) Gao <harrygao.oz@gmail.com>
2013-07-12 12:20:15 +1000

    create a nodejs express everyauth facebook example which can access the user information

---
+++ README.md
@@ -0,0 +1,6 @@
+node-express-everyauth-facebook-example
+=======================================
+
+This example is a simplied version of the https://github.com/ThomasHambach/node-express-everyauth-facebook-example
+
+However, that version has a problem to access the User information from everyauth. This example fixed that problem and showed how to use the user information in the view
---
+++ app.js
@@ -0,0 +1,91 @@
+var express = require('express')
+    , path = require('path')
+    , fs = require('fs')
+    , mongoose = require('mongoose')
+    , http = require('http')
+    , everyauth = require('everyauth');
+
+
+var UserSchema = mongoose.Schema({
+    name: String,
+    facebook_id: String,
+    facebook: {}
+});
+
+var UserModel = mongoose.model('UserModel', UserSchema);
+
+mongoose.connect('mongodb://127.0.0.1/mymongodb');
+
+everyauth.everymodule.findUserById(function(objId,callback) {
+     UserModel.findById(objId, function(err, user){
+        callback(null, user);
+    });
+});
+
+var findOrCreateFBUser = function(session, accessToken, accessTokExtra, fbUserMetadata){
+    var promise = this.Promise();
+        UserModel.findOne({facebook_id: fbUserMetadata.id},function(err, user) {
+            if (err) return promise.fulfill([err]);
+
+            if(user) {
+                promise.fulfill(user);
+            } else {
+                var User = new UserModel({
+                    name: fbUserMetadata.name,
+                    facebook_id: fbUserMetadata.id,
+                    facebook: fbUserMetadata
+                });
+
+                User.save(function(err,user) {
+                    if (err) return promise.fulfill([err]);
+                    promise.fulfill(user);
+                });
+            }
+        });
+        return promise;
+}
+
+everyauth.facebook
+    .appId('144772812392904')
+    .appSecret('90c2e3d9829758a8c6bbc6b0990146fe')
+    .scope('email,user_location,user_photos,publish_actions')
+    .handleAuthCallbackError( function (req, res) {
+        res.send('Error occured');
+    })
+    .findOrCreateUser(findOrCreateFBUser)
+    .redirectPath('/');
+
+/**
+ * Start and setup express
+ * @type {*}
+ */
+var app = express();
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('super secret'));
+  app.use(express.session());
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(everyauth.middleware(app)); // important to call this AFTER session!
+  app.use(app.router);
+
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
+
